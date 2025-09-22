89e07ec98d546a4c72d802ac61d83e892091f8b8
joaoliveirarruda <joaoliveirarruda@gmail.com>
2025-09-22 15:46:03 -0300

    main menu

--- main.py
+++ main.py
@@ -2,53 +2,23 @@ import pygame
 import random
 from scripts import entity, util, menu
 
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
-    clock = pygame.time.Clock()
-    running = True
-    pause = False
-
-    ASSETS = {
-        'imgs': {
-            'environment':{
-                'floor': pygame.image.load('data/imgs/environment/floor.png'),
-                'background': pygame.image.load('data/imgs/environment/background.png')
-            },
-            'player':{
-                'walk': (util.list_frames('player', 'walk'), 5, True),
-                'idle': (util.list_frames('player', 'idle'), 5, True),
-                'jump': (util.list_frames('player', 'jump'), 5, True),
-                'roll': (util.list_frames('player', 'roll'), 3, False),
-                'attk': (util.list_frames('player', 'attk'), 10, False),
-                'pray': (util.list_frames('player', 'pray'), 30, False)
-            }
-        },
-        'audio':{
-            'player': {
-                'jump': (util.sound('player', 'jump'), False)
-            }
-        }
-    }
-
+def game_loop(screen, clock, ASSETS):
     player = entity.get_player(ASSETS, (100, 475), (64, 64))
     movement = [False, False]
+    running = True
 
     while running:
 
         screen.blit(ASSETS["imgs"]['environment']['background'], (0,0))
         screen.blit(ASSETS["imgs"]['environment']['floor'], (0, 0))
         
-
-        
-
         entity.update_player(player, (movement[1] - movement[0], 0))
         entity.render_entity(player, screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
                     movement[0] = True
@@ -77,6 +47,51 @@ def main():
         pygame.display.flip()
     
         clock.tick(30)
+
+    return
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    
+    
+
+    ASSETS = {
+        'imgs': {
+            'environment':{
+                'floor': pygame.image.load('data/imgs/environment/floor.png'),
+                'background': pygame.image.load('data/imgs/environment/background.png')
+            },
+            'player':{
+                'walk': (util.list_frames('player', 'walk'), 5, True),
+                'idle': (util.list_frames('player', 'idle'), 5, True),
+                'jump': (util.list_frames('player', 'jump'), 5, True),
+                'roll': (util.list_frames('player', 'roll'), 3, False),
+                'attk': (util.list_frames('player', 'attk'), 10, False),
+                'pray': (util.list_frames('player', 'pray'), 30, False)
+            }
+        },
+        'audio':{
+            'player': {
+                'jump': (util.sound('player', 'jump'), False)
+            }
+        }
+    }
+
+    
+    while True:
+        choice = menu.main_menu(screen, clock)
+
+        if choice == "play":
+            game_loop(screen, clock, ASSETS)
+
+        if choice == "quit":
+            break
+
+
+    
 
     pygame.quit()
 
--- scripts/menu.py
+++ scripts/menu.py
@@ -1,4 +1,40 @@
 import pygame
 
+def main_menu(screen, clock): 
+    font = pygame.font.Font(None, 74)
+    pygame.display.set_caption("Mad and madder")
+
+    play_button = pygame.Rect(screen.get_width() / 2 - 100, 300, 200 , 50)
+    quit_button = pygame.Rect(screen.get_width() / 2 - 100, 400, 200, 50)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return "quit"
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.collidepoint(event.pos):
+                    return "play"
+                if quit_button.collidepoint(event.pos):
+                    return "quit"
+        
+        screen.fill((10, 20, 30))
+
+        tittle_text = font.render("Mad and madder", True, (255, 255, 255))
+        screen.blit(tittle_text, (screen.get_width() / 2 - tittle_text.get_width() / 2, 150))
+
+        pygame.draw.rect(screen, (0, 150, 0), play_button)
+        play_text = font.render("Play", True, (255, 255, 255))
+        screen.blit(play_text, (play_button.x + (play_button.width - play_text.get_width()) / 2, play_button.y))
+
+        pygame.draw.rect(screen, (150, 0, 0), quit_button)
+        quit_text = font.render('Sair', True, (255, 255, 255))
+        screen.blit(quit_text, (quit_button.x + (quit_button.width - quit_text.get_width()) / 2, quit_button.y))
+
+        pygame.display.flip()
+        clock.tick(30)
+
+
+
 def pause():
     pass
